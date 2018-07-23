7561e3ce0cfec0ec68d812c321437b6ed9dff8eb
Spencer Hann <spencer.hann@gmail.com>
2018-07-22 19:19:11 -0700

    hw4_complete

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,6 +1,10 @@
 from timeit import default_timer as timer;
 from math import inf
 
+# base test cases
+str1 = ["A","B","C","C","D"]
+str2 = ["A","E","B","F","C","D"]
+
 class Alignmt:
     def __init__(self, v=inf, s=""):
         # initialize 'best' score to 
@@ -14,9 +18,6 @@ class Alignmt:
     def __lt__(self, op):
         return self.score < op
 
-    def __eq__(self, op):
-        return self.score == op
-
     def __str__(self):
         return "%i %s" % (self.score, self.str)
     __repr__ = __str__
@@ -25,24 +26,12 @@ def version1(str1, str2, x=0, y=0, path=""):
     if x > len(str1) or y > len(str2):
         return
     if x == len(str1) and y == len(str2):
-        print(path)
+        #print(path)
         return
 
     version1(str1, str2, x+1, y, path + '-')
     version1(str1, str2, x, y+1, path + '|')
     version1(str1, str2, x+1, y+1, path + '\\')
-
-def exp_test():
-    lst = []
-    lst_ = []
-    print("n:\ttime:")
-    for i in range(10):
-        lst.append(i)
-        lst_.append(i)
-        start = timer();
-        version1(lst,lst_,0,0,"")
-        end = timer();
-        print(str(i+1) +"\t"+ str(end-start))
 
 def version2(str1, str2, x=0, y=0, path="", score=0):
     if x > len(str1) or y > len(str2):
@@ -104,11 +93,9 @@ def version4(input1, input2):
     for y in range(len(input2)):
         output[(-1,y)] = Alignmt(y+1,"|"*(y+1))
 
-    print("limits: (%i,%i)" % (len(input1),len(input2)))
-
     _version4(output, input1, input2, len(input1)-1, len(input2)-1)
-    display(output)
-    print(output[(len(input1)-1,len(input2)-1)])
+    #display(output)
+    #print(output[(len(input1)-1,len(input2)-1)])
 
 def _version4(arr, str1, str2, x=0, y=0):
     if x >= len(str1) or y >= len(str2):
@@ -132,13 +119,32 @@ def _version4(arr, str1, str2, x=0, y=0):
     else:
         arr[(x,y)] = Alignmt(diagon.score+1, diagon.str + "\\")
 
-    #display(arr)
     return arr[(x,y)]
 
-str1 = ["A","B","C","C","D"]
-str2 = ["A","E","B","F","C","D"]
+def exp_test(v, lower=1, upper=11, inc=1):
+    version = {
+        1 : version1,
+        2 : version2,
+        3 : version3,
+        4 : version4
+    }
+    lst = []; lst0 = []
+
+    print("n:\ttime:")
+    i = lower
+    while i < upper:
+        lst.append(i)
+        lst0.append(i)
+        start = timer();
+        version[v](lst,lst0)
+        end = timer();
+        t = (end-start)*1000
+        print(str(i) +"\t"+ str(t) + " ms")
+        i += inc #increment
+
 #version1(str1, str2)
 #version2(str1, str2)
 #version3(str1, str2)
-version4(str1, str2)
-#exp_test()
+#version4(str1, str2)
+exp_test(1)
+exp_test(4,lower=10,upper=501,inc=10)
